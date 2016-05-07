e0fd4e69a1e641294018d140ee42e31b1bb9c3da
Hugo Paez <hapj@outlook.cl>
2016-05-07 00:02:36 -0300

    Ultima revision del sitio web, 100% funcional

--- equipo.php
+++ equipo.php
@@ -1,1 +1,191 @@
-<!--Author: W3layoutsAuthor URL: http://w3layouts.comLicense: Creative Commons Attribution 3.0 UnportedLicense URL: http://creativecommons.org/licenses/by/3.0/--><!DOCTYPE HTML><html><head> <link rel="shortcut icon" type="image/x-icon" href="images/favico.ico"/> <title>Vuala Pole Studio - Equipo</title> <meta content="width=device-width, initial-scale=1.0" name="viewport"/> <meta content="width=device-width, initial-scale=1" name="viewport"/> <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/> <meta content="vuala pole studio, pole studio, pole, studio, facebook, vuala, sin gravedad, sin limites, yo puedo, vuala pole fitness, fitness" name="keywords"/> <link href="css/bootstrap.min.css" media="all" rel="stylesheet" type="text/css"/> <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/> <link href="css/style.min.css" media="all" rel="stylesheet" type="text/css"/> <link href="css/main.min.css" media="all" rel="stylesheet" type="text/css"/> <link href='http://fonts.googleapis.com/css?family=Sintony:400,700' rel='stylesheet' type='text/css'> <link href='http://fonts.googleapis.com/css?family=Dosis:200,300,400,500,600,700,800' rel='stylesheet' type='text/css'> <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script><script src="js/bootstrap.min.js"></script> <script type="text/javascript">$(document).ready(function(){$('.carousel').carousel({interval:10000});});</script></head><body> <div class="header"> <div class="container"> <div class="header-main"> <div class="logo"> <h2><a href="index"><span class="logo-clr">Vuala Pole Studio</span></a></h2> </div><div class="head-right"> <div class="top-nav"> <span class="menu"> <img alt="" src="images/icon.png"/></span> <ul class="res"> <li> <a href="index"> <span data-hover="Principal">Principal</span> </a> </li><li> <a class="active" href="equipo"> Equipo </a> </li><li> <a href="horario"> <span data-hover="Horario">Horario</span> </a> </li><li> <a href="galeria"> <span data-hover="Galería">Galería</span> </a> </li><li> <a href="ubicacion"> <span data-hover="Ubicación">Ubicación</span> </a> </li><li> <a href="contacto"> <span data-hover="Contacto">Contacto</span> </a> </li><div class="clearfix"></div></ul> <script>$("span.menu").click(function(){$("ul.res").slideToggle(300, function(){});}); </script> </div></div></div></div></div><div class="container-full background-gris "> <section id="slider" class="carousel slide"> <ol class="carousel-indicators"> <li data-target="#slider" data-slide-to="0" class="active"></li><li data-target="#slider" data-slide-to="1"></li><li data-target="#slider" data-slide-to="2"></li></ol> <div class="carousel-inner container-banner"> <div class="item active"> <img src="images/banner1.jpg" class="adaptar ubicacion1"> </div><div class="item"> <img src="images/banner2.jpg" class="adaptar ubicacion2"> </div><div class="item"> <img src="images/banner3.jpg" class="adaptar ubicacion3"> </div></div><a href="#slider" class="left carousel-control" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a> <a href="#slider" class="right carousel-control" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a> </section> </div><div class="recent-posts"> <div class="container"> <div class="recent-bottom"> <div class="col-md-12 recent-left"> <h3>Nuestro Equipo</h3><hr> <div class="col-xs-12 col-sm-12 col-md-12 col-lg-20"> <div class="row"> <div class="hidden-xs col-sm-6 col-md-6 col-lg-3"> <img src="images/pizarro.jpg" class="img-responsive"> </div><div class="col-xs-12 col-sm-9 col-md-6 col-lg-7"> <h3>Fernanda Pizarro Castro</h3><hr> <p align="justify"> Directora de Vualá Pole Studio<br>Nivel Básico: Obtenido en Allegra Pole Dance con Ana Brito de Buenos Aires Pole Studio.<br>Nivel Intermedio - Avanzado: Alma Pole, Body & Soul, con Viviana Brown, Paloma Velasco y Ricardo Bustos.<br>Publicista </p></div></div></div><br><br><div class="col-xs-12 col-sm-12 col-md-12 col-lg-20"> <div class="row"> <div class="hidden-xs col-sm-6 col-md-6 col-lg-3"> <img src="images/vargas.jpg" class="img-responsive"> </div><div class="col-xs-12 col-sm-9 col-md-6 col-lg-7"> <h3>Alisson Vargas Caamaño</h3><hr> <p align="justify"> Instructora nivel Básico - Intermedio - Avanzado<br>Avalada por Vuala Pole Studio<br>Estudiante de Odontología </p></div></div></div><br><br><div class="col-xs-12 col-sm-12 col-md-12 col-lg-20"> <div class="row"> <div class="hidden-xs col-sm-6 col-md-6 col-lg-3"> <img src="images/camargo.jpg" class="img-responsive"> </div><div class="col-xs-12 col-sm-9 col-md-6 col-lg-7"> <h3>Fernanda Camargo Aragón</h3><hr> <p align="justify"> Instructora nivel Básico - Intermedio<br>Avalada por Vuala Pole Studio<br>Profesora de Educación Física con Mención en Adulto Mayor </p></div></div></div></div><div class="clearfix"></div></div></div><div class="clearfix"></div></div><div class="footer"> <div class="container"> <div class="footer-main"> <div class="footer-top"> <div class="col-md-6 footer-news"> <h5>Copyright</h5> <hr> <h4>&copy; Todos los derechos a Programadores.cl</h4> <h4>Modificado por GH - Soluciones Informáticas</h4> <br></div><div class="col-md-6 footer-news .col-md-offset-4"> <h5>Contáctanos</h5> <hr> <h4><a href="https://www.facebook.com/Vual%C3%A1-Pole-Studio-415125465349716/?fref=ts">Facebook</a></h4> <h4><a href="contacto.php">Correo Electrónico</a></h4> <h4>+56 9 9330 3572</h4> </div><div class="clearfix"></div></div></div></div></div></body></html>
+<!--Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+	<head>
+    	<link rel="shortcut icon" type="image/x-icon" href="images/favico.ico" />
+        <title>Vuala Pole Studio - Equipo</title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+        <meta content="width=device-width, initial-scale=1" name="viewport"/>
+        <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+        <meta content="vuala pole studio, pole studio, pole, studio, facebook, vuala, sin gravedad, sin limites, yo puedo, vuala pole fitness, fitness" name="keywords"/>
+        <!-- hojas de estilo -->
+        <link href="css/bootstrap.min.css" media="all" rel="stylesheet" type="text/css"/>
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/style.min.css" media="all" rel="stylesheet" type="text/css"/>
+        <link href="css/main.min.css" media="all" rel="stylesheet" type="text/css"/>
+        <!--Google Fonts-->
+        <link href='http://fonts.googleapis.com/css?family=Sintony:400,700' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Dosis:200,300,400,500,600,700,800' rel='stylesheet' type='text/css'>
+        <!-- JavaScript -->
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+        <script type="text/javascript">
+			$(document).ready(function(){
+				$('.carousel').carousel({
+					interval:10000
+				});
+			});
+		</script>
+	</head>
+	<body>
+        <!--header start here-->
+        <div class="header">
+            <div class="container">
+                <div class="header-main">
+                    <div class="logo">
+                        <h2><a href="index"><span class="logo-clr">Vuala Pole Studio</span></a></h2>
+                    </div>
+                    <div class="head-right">
+                        <div class="top-nav"> <span class="menu"> <img alt="" src="images/icon.png"/></span>
+                            <ul class="res">
+                                <li>
+                                	<a href="index"> <span data-hover="Principal">Principal</span> </a>
+                                </li>
+                                <li>
+                                	<a class="active" href="equipo"> Equipo </a>
+                                </li>
+                                <li>
+                                	<a href="horario"> <span data-hover="Horario">Horario</span> </a>
+                                </li>
+                                <li>
+                                	<a href="galeria"> <span data-hover="Galería">Galería</span> </a>
+                                </li>
+                                <li>
+                                	<a href="ubicacion"> <span data-hover="Ubicación">Ubicación</span> </a>
+                                </li>
+                                <li>
+                                	<a href="contacto"> <span data-hover="Contacto">Contacto</span> </a>
+                                </li>
+                                <div class="clearfix"></div>
+                            </ul>
+                            <!-- script-for-menu --> 
+                            <script>
+                                $("span.menu").click(function() {
+                                    $("ul.res").slideToggle(300, function() {});
+                                });
+                            </script> 
+                            <!-- /script-for-menu --> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--header end here--> 
+        <!--banner start here-->
+        <div class="container-full background-gris ">
+        	<section id="slider" class="carousel slide">
+                <ol class="carousel-indicators">
+                    <li data-target="#slider" data-slide-to="0" class="active"></li>
+                    <li data-target="#slider" data-slide-to="1"></li>
+                    <li data-target="#slider" data-slide-to="2"></li>
+                </ol>
+
+                <div class="carousel-inner container-banner">
+                    <div class="item active">
+                        <img src="images/banner1.jpg" class="adaptar ubicacion1">
+                    </div>
+                    <div class="item">
+                        <img src="images/banner2.jpg" class="adaptar ubicacion2">
+                    </div>
+                    <div class="item">
+                        <img src="images/banner3.jpg" class="adaptar ubicacion3">
+                    </div>
+                </div>
+
+                <a href="#slider" class="left carousel-control" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+                <a href="#slider" class="right carousel-control" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+            </section>
+        </div>
+        
+        <!--banner end here-->
+        <div class="recent-posts">
+            <div class="container">
+                <div class="recent-bottom">
+                    <div class="col-md-12 recent-left">
+                    	<h3>Nuestro Equipo</h3><hr>
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-20">
+                            <div class="row">
+                                <div class="hidden-xs col-sm-6 col-md-6 col-lg-3">
+                                    <img src="images/pizarro.jpg" class="img-responsive">
+                                </div>
+                                <div class="col-xs-12 col-sm-9 col-md-6 col-lg-7">
+                                    <h3>Fernanda Pizarro Castro</h3><hr>
+                                    <p align="justify">
+                                        Directora de Vualá Pole Studio<br>
+                                        Nivel Básico: Obtenido en Allegra Pole Dance con Ana Brito de Buenos Aires Pole Studio.<br>
+                                        Nivel Intermedio - Avanzado: Alma Pole, Body & Soul, con Viviana Brown, Paloma Velasco y Ricardo Bustos.<br>
+                                        Publicista
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <br>
+                        <br>
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-20">
+                            <div class="row">
+                                <div class="hidden-xs col-sm-6 col-md-6 col-lg-3">
+                                    <img src="images/vargas.jpg" class="img-responsive">
+                                </div>
+                                <div class="col-xs-12 col-sm-9 col-md-6 col-lg-7">
+                                    <h3>Alisson Vargas Caamaño</h3><hr>
+                                    <p align="justify">
+                                        Instructora nivel Básico - Intermedio - Avanzado<br>
+                                        Avalada por Vuala Pole Studio<br>
+                                        Estudiante de Odontología
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <br>
+                        <br>
+                        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-20">
+                            <div class="row">
+                                <div class="hidden-xs col-sm-6 col-md-6 col-lg-3">
+                                    <img src="images/camargo.jpg" class="img-responsive">
+                                </div>
+                                <div class="col-xs-12 col-sm-9 col-md-6 col-lg-7">
+                                    <h3>Fernanda Camargo Aragón</h3><hr>
+                                    <p align="justify">
+                                        Instructora nivel Básico - Intermedio<br>
+                                        Avalada por Vuala Pole Studio<br>
+                                        Profesora de Educación Física con Mención en Adulto Mayor
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+        <!--footer star here-->
+        <div class="footer">
+            <div class="container">
+                <div class="footer-main">
+                    <div class="footer-top">
+                        <div class="col-md-6 footer-news">
+                            <h5>Copyright</h5>
+                            <hr>
+                            <h4>&copy; Todos los derechos a Programadores.cl</h4>
+                            <h4>Modificado por GH - Soluciones Informáticas</h4>
+                            <br>
+                        </div>
+                        <div class="col-md-6 footer-news .col-md-offset-4">
+                            <h5>Contáctanos</h5>
+                            <hr>
+                            <h4><a href="https://www.facebook.com/Vual%C3%A1-Pole-Studio-415125465349716/?fref=ts">Facebook</a></h4>
+                            <h4><a href="contacto.php">Correo Electrónico</a></h4>
+                            <h4>+56 9 9330 3572</h4>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+	</body>
+</html>
